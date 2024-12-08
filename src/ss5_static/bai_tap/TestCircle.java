88c91b06e4f3effb6239193b9807330420f774a4
hoangthai203 <hoangthai16122003@gmail.com>
2024-12-08 13:21:00 +0700

    Nộp bài

---
+++ src/ss5_static/bai_tap/TestCircle.java
@@ -0,0 +1,15 @@
+package ss5_static.bai_tap;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle(5.0);
+
+        System.out.println("Circle 1 - Radius: " + circle1.getRadius());
+        System.out.println("Circle 1 - Area: " + circle1.getArea());
+
+        System.out.println("Circle 2 - Radius: " + circle2.getRadius());
+        System.out.println("Circle 2 - Area: " + circle2.getArea());
+    }
+}
+
---
+++ src/ss5_static/bai_tap/TestStudent.java
@@ -0,0 +1,13 @@
+package ss5_static.bai_tap;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setName("Alice");
+        student.setClasses("C03");
+
+        student.displayInfo();
+    }
+}
+
